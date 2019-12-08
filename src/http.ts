dafa787923cf9179f5233c90243cb5bc60530881
WMXPY <wm@mengw.io>
2019-12-07 18:02:10 -0600

    add comments

--- src/http.ts
+++ src/http.ts
@@ -6,20 +6,24 @@
 
 export enum HTTP_RESPONSE_CODE {
 
+    // Information
     CONTINUE = 100,
     SWITCHING_PROTOCOLS = 101,
 
+    // Successful
     OK = 200,
     CREATED = 201,
     ACCEPTED = 202,
     NO_CONTENT = 204,
 
+    // Redirection
     MOVED_PERMANENTLY = 301,
     FOUND = 302,
     SEE_OTHER = 303,
     NOT_MODIFIED = 304,
     TEMPORARY_REDIRECT = 307,
 
+    // Client Error
     BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
     FORBIDDEN = 403,
@@ -29,6 +33,7 @@ export enum HTTP_RESPONSE_CODE {
     PRECONDITION_FAILED = 412,
     UNSUPPORTED_MEDIA_TYPE = 415,
 
+    // Server Error
     INTERNAL_SERVER_ERROR = 500,
     NOT_IMPLEMENTED = 501,
 }
