76d90a9175c977b1869adc22e7458df0a26159dc
PCXPY <pc@wmpcxpy.com>
2020-03-02 11:03:04 -0600

    [feat] impleent time builder

--- src/time/builder.ts
+++ src/time/builder.ts
@@ -1,3 +1,5 @@
+import { TIME_IN_MILLISECONDS } from "./time";
+
 /**
  * @author WMXPY
  * @namespace Magic_Time
@@ -19,16 +21,39 @@ export type TimeBuilderArgs = {
 export class TimeBuilder {
 
     public static from(args: TimeBuilderArgs): TimeBuilder {
-
-        return new TimeBuilder();
+        return new TimeBuilder(args);
     }
 
-    private _year?: number;
-    private _month?: number;
-    private _week?: number;
-    private _day?: number;
-    private _hour?: number;
-    private _minute?: number;
-    private _second?: number;
-    private _millisecond?: number;
+    private readonly _year: number;
+    private readonly _month: number;
+    private readonly _week: number;
+    private readonly _day: number;
+    private readonly _hour: number;
+    private readonly _minute: number;
+    private readonly _second: number;
+    private readonly _millisecond: number;
+
+    private constructor(args: TimeBuilderArgs) {
+
+        this._year = args.year ?? 0;
+        this._month = args.month ?? 0;
+        this._week = args.week ?? 0;
+        this._day = args.day ?? 0;
+        this._hour = args.hour ?? 0;
+        this._minute = args.minute ?? 0;
+        this._second = args.second ?? 0;
+        this._millisecond = args.millisecond ?? 0;
+    }
+
+    public inMilliseconds(): number {
+
+        return TIME_IN_MILLISECONDS.YEAR * this._year
+            + TIME_IN_MILLISECONDS.MONTH * this._month
+            + TIME_IN_MILLISECONDS.WEEK * this._week
+            + TIME_IN_MILLISECONDS.DAY * this._day
+            + TIME_IN_MILLISECONDS.HOUR * this._hour
+            + TIME_IN_MILLISECONDS.MINUTE * this._minute
+            + TIME_IN_MILLISECONDS.SECOND * this._second
+            + this._millisecond;
+    }
 }
