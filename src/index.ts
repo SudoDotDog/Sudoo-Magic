ad3c50dd834371a3746c644f70b798a3455a5b2c
WMXPY <wm@mengw.io>
2020-04-10 15:17:43 -0500

    init mimetype

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@
 export * from "./binary";
 export * from "./distance";
 export * from "./http";
+export * from "./mimetype";
 export * from './port';
 export * from "./time/builder";
 export * from "./time/time";
---
+++ src/mimetype.ts
@@ -0,0 +1,9 @@
+/**
+ * @author WMXPY
+ * @namespace Magic
+ * @description Mimetype
+ */
+
+export enum MIMETYPE {
+
+}
