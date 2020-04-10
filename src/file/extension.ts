95a6b8cb8b19f578a8439b5e3a213e487ff22172
WMXPY <wm@mengw.io>
2020-04-10 15:22:36 -0500

    add more file type

--- src/file/extension.ts
+++ src/file/extension.ts
@@ -6,5 +6,15 @@
 
 export enum FILE_EXTENSION {
 
+    APPLICATION_PDF = '.pdf',
+    APPLICATION_ZIP = '.zip',
+    APPLICATION_ZIP_COMPRESSED = '.zip',
+
+    IMAGE_GIF = '.gif',
     IMAGE_JPEG = '.jpeg',
+    IMAGE_PNG = '.png',
+    IMAGE_WEBP = '.webp',
+
+    TEXT_CSV = '.csv',
+    TEXT_PLAIN = '.txt',
 }
--- src/file/mimetype.ts
+++ src/file/mimetype.ts
@@ -6,5 +6,15 @@
 
 export enum MIMETYPE {
 
+    APPLICATION_PDF = 'application/pdf',
+    APPLICATION_ZIP = 'application/zip',
+    APPLICATION_ZIP_COMPRESSED = 'application/x-zip-compressed',
+
+    IMAGE_GIF = 'image/gif',
     IMAGE_JPEG = 'image/jpeg',
+    IMAGE_PNG = 'image/png',
+    IMAGE_WEBP = 'image/webp',
+
+    TEXT_CSV = 'text/csv',
+    TEXT_PLAIN = 'text/plain',
 }
